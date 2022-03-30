acf07a3917c1803c88f648e930d4f7dd37702aa1
Maria Manoela <manoelamvc@hotmail.com>
2022-03-29 21:02:49 -0300

    Create array of objects

--- src/components/Lista/index.tsx
+++ src/components/Lista/index.tsx
@@ -1,6 +1,16 @@
 import React from "react";
 
 function Lista() {
+    const tarefas = [
+        {
+            tarefa: 'React',
+            tempo: '02:00:00'
+        },
+        {
+            tarefa: 'Javascript',
+            tempo: '01:00:00'
+        }
+    ]
     return (
         <aside>
             <h2>Estudos do dia</h2>
